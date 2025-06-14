e5649e069d218c96324921df8f4a77d041708998
AldoVasquez07 <142806675+AldoVasquez07@users.noreply.github.com>
2025-06-13 21:46:10 -0500

    Update wsgi.py

--- cs_lib_oauth/wsgi.py
+++ cs_lib_oauth/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs_lib_oauth.settings')
 
 application = get_wsgi_application()
+
+app = application
